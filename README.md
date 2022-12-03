992508dbe22bd8e154d5719d49484417dc0168ed
Fredrik Rose <fredrik.rose.developer@gmail.com>
2022-12-03 09:01:33 +0100

    Add solution for day 3

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 Solutions for the advent of code 2022 puzzles, implemented in Python. The
 puzzles can be found here: https://adventofcode.com/2022
+
+## Python
+
+Iterate a list in groups of size `N`:
+
+```
+for group in (items[i:i + N] for i in range(0, len(items), N)):
+    pass
+```
---
+++ adventofcode/day03/3.py
@@ -0,0 +1,49 @@
+# Day 3: Rucksack Reorganization
+
+
+def main():
+    packs = parse("3.txt")
+    part_one(packs.copy())
+    part_two(packs.copy())
+
+
+def parse(file):
+    packs = []
+    with open(file) as file:
+        packs = [line.strip() for line in file]
+    return packs
+
+
+def part_one(packs):
+    common_items = []
+    for pack in packs:
+        half = len(pack) // 2
+        common = set(pack[:half]).intersection(pack[half:])
+        assert len(common) == 1
+        common_items.append(list(common)[0])
+    priority_sum = get_priority(common_items)
+    print(f"Part one: {priority_sum}")
+
+
+def part_two(packs):
+    common_items = []
+    for group in (packs[i:i + 3] for i in range(0, len(packs), 3)):
+        common = set(group[0]).intersection(*group[1:])
+        assert len(common) == 1
+        common_items.append(list(common)[0])
+    priority_sum = priority_sum = get_priority(common_items)
+    print(f"Part two: {priority_sum}")
+
+
+def get_priority(items):
+    priority_sum = 0
+    for item in items:
+        if item.isupper():
+            priority_sum += ord(item) - 38
+        else:
+            priority_sum += ord(item) - 96
+    return priority_sum
+
+
+if __name__ == "__main__":
+    main()
